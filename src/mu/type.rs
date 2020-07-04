327f440f56aa6c2e4d3bd417c249e6c81a25afdf
putnamjm <putnamjm@sa.edu>
2020-07-03 22:51:45 -0400

    #5 - env/mu module

--- src/mu/type.rs
+++ src/mu/type.rs
@@ -126,29 +126,8 @@ impl Type {
     pub fn eq(&self, ptr: Type) -> bool {
         return self.bits == ptr.bits;
     }
+
+    pub fn is_null(&self) -> bool {
+        return self.eq(NIL);
+    }
 }
-
-/***
-template <typename T>
-static T* Untag(TagPtr ptr) {
-    return reinterpret_cast<T*>(to_underlying(ptr) & ~0x7);
-}
-
-static constexpr TagPtr T = static_cast<TagPtr>(
-    ('t' << 8) | ((1 & 0x7) << 5) |
-    ((static_cast<uint8_t>(IMMEDIATE_CLASS::KEYWORD) & 0x3) << 3) |
-    (static_cast<uint8_t>(TAG::IMMEDIATE) & 0x7));
-
-static constexpr TagPtr NIL = static_cast<TagPtr>(
-    ((('l' << 16) | ('i' << 8) | 'n') << 8) | ((3 & 0x7) << 5) |
-    ((static_cast<uint8_t>(IMMEDIATE_CLASS::KEYWORD) & 0x3) << 3) |
-    (static_cast<uint8_t>(TAG::IMMEDIATE) & 0x7));
-
-  static SYS_CLASS TypeOf(TagPtr);
-  static bool IsClassSymbol(TagPtr);
-  static TagPtr MapClassSymbol(SYS_CLASS);
-  static SYS_CLASS MapSymbolClass(TagPtr);
-
-  static constexpr TagPtr BoolOf(bool test) { return test ? T : NIL; }
-  static constexpr bool Null(TagPtr ptr) { return Eq(ptr, NIL); }
-***/
