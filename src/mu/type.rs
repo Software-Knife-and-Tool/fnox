33999383e0c07cb50ff2cc7e5f4044ce50b9b6bb
putnamjm <putnamjm@sa.edu>
2020-07-03 21:51:52 -0400

    #5 - env/mu module

--- src/mu/type.rs
+++ src/mu/type.rs
@@ -16,7 +16,7 @@ pub enum Tag {
     Extend = 7     /* extended */
 }
 
-enum SysClass {
+pub enum SysClass {
     Byte,
     Char,
     Code,
@@ -40,23 +40,75 @@ enum SysClass {
     View
 }
 
-enum ImmediateClass {
+/** * immediate pointer layout: [d*].lllttTTT **/
+#[derive(FromPrimitive)]
+pub enum ImmediateClass {
     Char = 0,
     String = 1,
     Keyword = 2,
     Float = 3
 }
 
-const IMMEDIATE_STR_MAX: u32 = 7;
+const IMMEDIATE_STR_MAX: u64 = 7;
 
 impl Type {
-    pub fn tag_of(&self) -> Tag {
-        let element: std::option::Option<Tag> =
+    pub fn tag(&self) -> Tag {
+        let tag: std::option::Option<Tag> =
             num::FromPrimitive::from_u64(self.bits & 0x7);
-        match element {
-            Some(_) => element.unwrap(),
+        match tag {
+            Some(_) => tag.unwrap(),
             None => panic!("Unknown tag")
         }
+    }
+
+    pub fn entag(base: u64, tag: Tag) -> Type {
+        Type {
+            bits: base | tag as u64
+        }
+    }
+
+    pub fn immediate(data: u64, len: u8, tag: ImmediateClass) -> Type {
+        Type {
+            bits: (data << 8)
+                | ((len as u64) << 5)
+                | ((tag as u64) << 3)
+                | ((Tag::Immediate as u64))
+        }
+    }
+
+    pub fn immediate_data(&self) -> u64 {
+        return self.bits >> 8;
+    }
+
+    pub fn immediate_size(&self) -> u64 {
+        return self.bits >> 5;
+    }
+    
+    pub fn immediate_class(&self) -> ImmediateClass {
+        let tag: std::option::Option<ImmediateClass> =
+            num::FromPrimitive::from_u64(self.bits >> 3);
+        match tag {
+            Some(_) => tag.unwrap(),
+            None => panic!("Unknown tag")
+        }
+    }
+
+    pub fn is_immediate(&self) -> bool {
+        match self.tag() {
+            Tag::Immediate => true,
+            _ => false
+        }
+    }
+  
+    pub fn is_extended(&self) -> bool {
+        match self.tag() {
+            Tag::Extend => true,
+            _ => false
+        }
+    }
+
+    pub fn eq(&self, ptr: Type) -> bool {
+        return self.bits == ptr.bits;
     }
 }
 
@@ -64,45 +116,6 @@ impl Type {
 template <typename T>
 static T* Untag(TagPtr ptr) {
     return reinterpret_cast<T*>(to_underlying(ptr) & ~0x7);
-}
-
-static constexpr TagPtr Entag(TagPtr ptr, TAG tag) {
-    return static_cast<TagPtr>(to_underlying(ptr) | to_underlying(tag));
-}
-
-static TagPtr Entag(void* caddr, TAG tag) {
-    return static_cast<TagPtr>(reinterpret_cast<u64_t>(caddr) |
-                               to_underlying(tag));
-}
-
-static void* ToAddress(TagPtr ptr) {
-    return reinterpret_cast<void*>(to_underlying(ptr));
-}
-
-static TagPtr FromU64(u64_t ptr) { return static_cast<TagPtr>(ptr); }
-
-static constexpr u64_t ToU64(TagPtr ptr) { return to_underlying(ptr); }
-
-
-/** * immediate pointer layout: [d*].lllttTTT **/
-static constexpr TagPtr MakeImmediate(u64_t data, size_t len,
-                                      IMMEDIATE_CLASS tag) {
-    return static_cast<TagPtr>(((data << 8) | ((len & 0x7) << 5) |
-                                ((static_cast<uint8_t>(tag) & 0x3) << 3) |
-                                (static_cast<uint8_t>(TAG::IMMEDIATE) & 0x7)));
-}
-
-static constexpr u64_t ImmediateData(TagPtr ptr) {
-    return static_cast<u64_t>(ptr) >> 8;
-}
-
-static constexpr size_t ImmediateSize(TagPtr ptr) {
-    return static_cast<size_t>((static_cast<u64_t>(ptr) >> 5) & 0x7);
-}
-
-static constexpr IMMEDIATE_CLASS ImmediateClass(TagPtr ptr) {
-    return static_cast<IMMEDIATE_CLASS>((static_cast<u64_t>(ptr) >> 3) &
-                                        0x3);
 }
 
 static constexpr TagPtr T = static_cast<TagPtr>(
@@ -115,22 +128,11 @@ static constexpr TagPtr NIL = static_cast<TagPtr>(
     ((static_cast<uint8_t>(IMMEDIATE_CLASS::KEYWORD) & 0x3) << 3) |
     (static_cast<uint8_t>(TAG::IMMEDIATE) & 0x7));
 
-
   static SYS_CLASS TypeOf(TagPtr);
   static bool IsClassSymbol(TagPtr);
   static TagPtr MapClassSymbol(SYS_CLASS);
   static SYS_CLASS MapSymbolClass(TagPtr);
 
   static constexpr TagPtr BoolOf(bool test) { return test ? T : NIL; }
-
-  static constexpr bool IsImmediate(TagPtr ptr) {
-    return TagOf(ptr) == TAG::IMMEDIATE;
-  }
-  
-  static constexpr bool IsExtended(TagPtr ptr) {
-    return TagOf(ptr) == TAG::EXTEND;
-  }
-  
-  static constexpr bool Eq(TagPtr p0, TagPtr p1) { return p0 == p1; }
   static constexpr bool Null(TagPtr ptr) { return Eq(ptr, NIL); }
 ***/
