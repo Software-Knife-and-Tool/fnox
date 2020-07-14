1f86245d585bf1abd4aae8cf4f245c4159c9c26f
James M. Putnam <putnamjm.design@gmail.com>
2020-07-14 10:46:53 -0400

    #19 - mu/fixnum module

--- src/mu/heap.rs
+++ src/mu/heap.rs
@@ -1,11 +1,12 @@
 /* mu/heap.rs */
-use crate::mu::r#type::Tag;
-use crate::mu::r#type::Type;
-use crate::mu::r#type::entag;
+// use crate::mu::r#type::Tag;
+// use crate::mu::r#type::Type;
+// use crate::mu::r#type::entag;
 
 pub struct Heap {
     size: u64,
-    fname: &'static str
+    fname: &'static str,
+    mmap: memmap::MmapMut
 }
 
 use memmap; // 0.7.0
@@ -14,38 +15,42 @@ use std::{
     io::{Seek, SeekFrom, Write},
 };
 
-/*
-const SIZE: u64 = 1024 * 1024;
-
-fn mmmap(size: u64, fn: str) -> u64 {
-    let src = "Hello!";
-
+fn mmap(size: u64, fname: &str) -> memmap::MmapMut {
     let mut f = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
-        .open("test.mmap")
+        .open(fname)
         .expect("Unable to open file");
 
-    -- Allocate space in the file first
-    f.seek(SeekFrom::Start(SIZE)).unwrap();
+    // Allocate space in the file first
+    f.seek(SeekFrom::Start(size)).unwrap();
     f.write_all(&[0]).unwrap();
     f.seek(SeekFrom::Start(0)).unwrap();
 
-    let mut data = unsafe {
+    // let src = "Hello!";
+    // let mut data = 
+    //    unsafe {
+    //        memmap::MmapOptions::new()
+    //            .map_mut(&f)
+    //            .expect("Could not access data from memory mapped file")
+    //    }
+    //
+    // data[..src.len()].copy_from_slice(src.as_bytes());
+
+    unsafe {
         memmap::MmapOptions::new()
             .map_mut(&f)
             .expect("Could not access data from memory mapped file")
-    };
-
-    data[..src.len()].copy_from_slice(src.as_bytes());
+    }
 }
-*/
 
 pub fn heap(words: u64) -> Heap {
+    println!("making heap, damnit");
     Heap {
         size: words,
-        fname: "hoopty"
+        fname: "hoopty",
+        mmap: mmap(1024 * 1024, "hoopty")
     }
 }
 
