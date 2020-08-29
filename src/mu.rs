60753d4f2b8c74c9c186df5f86080de02e3da982
putnamjm <putnamjm@sa.edu>
2020-08-28 22:33:29 -0400

    #26 - mu read, print modules

--- src/mu.rs
+++ src/mu.rs
@@ -2,5 +2,7 @@
 pub mod cons;
 pub mod env;
 pub mod fixnum;
+pub mod print;
+pub mod read;
 // pub mod heap;
 pub mod r#type;
---
+++ src/mu/read.rs
@@ -0,0 +1,39 @@
+/* mu/read.rs */
+// use std::io;
+
+// use crate::mu::r#type::Tag;
+use crate::mu::r#type::Type;
+use crate::mu::r#type::NIL;
+// xxuse crate::mu::r#type::entag;
+
+pub fn _read(_src: Type) -> Type {
+    NIL
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    /*
+    #[test]
+    fn test_type() {
+        assert!(fixnum(0).type_fixnum());
+    }
+
+    #[test]
+    fn test_u64() {
+        assert!(fixnum(0).u64_from_fixnum() == 0);
+        assert!(fixnum(1).u64_from_fixnum() == 1);
+    }
+
+    #[test]
+    fn test_eq() {
+        assert!(fixnum(0).eq(fixnum(0)));
+    }
+    
+    #[test]
+    fn test_add() {
+        assert!(fixnum(1).fixnum_add(fixnum(2)).eq(fixnum(3)));
+    }
+     */
+}
