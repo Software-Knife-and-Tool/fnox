1d4c8dcef0530bc44d810cd9a9b2cf1a297bf1ea
James M. Putnam <putnamjm.design@gmail.com>
2020-08-29 11:55:14 -0400

    #38 - sexpr parser

--- src/mu/fixnum.rs
+++ src/mu/fixnum.rs
@@ -1,19 +1,51 @@
 /* mu/fixnum.rs */
 use crate::mu::r#type::Tag;
 use crate::mu::r#type::Type;
+use crate::mu::r#type::NIL;
 use crate::mu::r#type::entag;
 
 struct _Fixnum {
     integer: i64
 }
 
-pub fn fixnum(src: u64) -> Type {
-    return entag(src << 2, Tag::Efixnum); 
+pub fn _fixnum(src: i64) -> Type {
+    entag((src as u64) << 2, Tag::Efixnum)
+}
+
+pub fn _fixnum_add(fx0: Type, fx1: Type) -> Type {
+    match _Fixnum::_from_type(fx0) {
+        Some(fx) =>
+            match fx._add(fx1) {
+                Some(_sum) =>  _sum,
+                None => NIL
+            },
+        None => NIL
+    }
 }
 
 impl _Fixnum {
-    pub fn _print(_type: Type) {
-        println!("{}", Type::u64_from_fixnum(&_type));
+    pub fn _print(&self) {
+        println!("{}", self.integer);
+    }
+    
+    pub fn _from_i64(_integer: i64) -> _Fixnum {
+        _Fixnum { integer: _integer as i64 }
+    }
+
+    pub fn _from_type(_type: Type) -> Option<_Fixnum> {
+        if Type::type_fixnum(&_type) {
+            Some(_Fixnum { integer: (_type.bits >> 2) as i64 })
+        } else {
+            None
+        }
+    }
+
+    pub fn _add(&self, fx: Type) -> Option<Type> {
+        if Type::type_fixnum(&fx) {
+            Some(_fixnum(self.integer + (fx.bits >> 2) as i64))
+        } else {
+            None
+        }
     }
 }
 
@@ -25,13 +57,13 @@ impl Type {
             _ => false
         }
     }
-
-    pub fn u64_from_fixnum(&self) -> u64 {
-        self.bits >> 2
-    }
-
-    pub fn fixnum_add(&self, fx: Type) -> Type {
-        fixnum(self.u64_from_fixnum() + fx.u64_from_fixnum())
+    
+    pub fn i64_from_fixnum(&self) -> Option<i64> {
+        if Type::type_fixnum(self) {
+            Some((self.bits >> 2) as i64)
+        } else {
+            None
+        }
     }
 }
 
@@ -41,22 +73,45 @@ mod tests {
 
     #[test]
     fn test_type() {
-        assert!(fixnum(0).type_fixnum());
+        assert!(_fixnum(0).type_fixnum());
     }
 
     #[test]
-    fn test_u64() {
-        assert!(fixnum(0).u64_from_fixnum() == 0);
-        assert!(fixnum(1).u64_from_fixnum() == 1);
+    fn test_i64() {
+        assert!(
+            match _fixnum(0).i64_from_fixnum() {
+                None => false,
+                Some(v) => v == 0
+            });
+
+        assert!(
+            match _fixnum(1).i64_from_fixnum() {
+                None => false,
+                Some(v) => v == 1
+            });
     }
 
     #[test]
     fn test_eq() {
-        assert!(fixnum(0).eq(fixnum(0)));
+        assert!(_fixnum(0).eq(_fixnum(0)));
+        assert!(!_fixnum(0).eq(_fixnum(1)));
     }
-    
+
     #[test]
     fn test_add() {
-        assert!(fixnum(1).fixnum_add(fixnum(2)).eq(fixnum(3)));
+        assert!(
+            match _Fixnum::_from_type(_fixnum(1)) {
+                Some(fx) =>
+                    match fx._add(_fixnum(2)) {
+                        Some(sum) =>
+                            match sum.i64_from_fixnum() {
+                                Some(v) => v == 3,
+                                None => false
+                            },
+                        None => false
+                    },
+                None => false
+            });
+        assert!(_fixnum_add(_fixnum(1), _fixnum(2)).eq(_fixnum(3)));
     }
 }
