ffb72054d1642b5c6388910fe68f95279657602a
James M. Putnam <putnamjm.design@gmail.com>
2020-08-29 21:12:34 -0400

    #44 - add symbol table

--- src/mu/env.rs
+++ src/mu/env.rs
@@ -36,3 +36,53 @@ impl Env<'_> {
     }
 }
 
+#[cfg(test)]
+mod tests {
+/*
+    use super::*;
+
+    #[test]
+    fn test_type() {
+        assert!(_fixnum(0).type_fixnum());
+    }
+
+    #[test]
+    fn test_i64() {
+        assert!(
+            match _fixnum(0).i64_from_fixnum() {
+                None => false,
+                Some(v) => v == 0
+            });
+
+        assert!(
+            match _fixnum(1).i64_from_fixnum() {
+                None => false,
+                Some(v) => v == 1
+            });
+    }
+
+    #[test]
+    fn test_eq() {
+        assert!(_fixnum(0).eq(_fixnum(0)));
+        assert!(!_fixnum(0).eq(_fixnum(1)));
+    }
+
+    #[test]
+    fn test_add() {
+        assert!(
+            match _Fixnum::_from_type(&_fixnum(1)) {
+                Some(fx) =>
+                    match fx._add(&_fixnum(2)) {
+                        Some(sum) =>
+                            match sum.i64_from_fixnum() {
+                                Some(v) => v == 3,
+                                None => false
+                            },
+                        None => false
+                    },
+                None => false
+            });
+    }
+*/
+}
+
