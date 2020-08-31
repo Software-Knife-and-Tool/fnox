141a3b4e272635d8bf58c953a7913d8cec6e56b8
James M. Putnam <putnamjm.design@gmail.com>
2020-08-30 20:45:34 -0400

    #44 - add symbol table

--- src/mu/env.rs
+++ src/mu/env.rs
@@ -3,27 +3,34 @@ use std::collections::HashMap;
 use crate::mu::r#type::SysClass;
 // use crate::mu::r#type::Tag;
 use crate::mu::r#type::Type;
-// use crate::mu::r#type::NIL;
+use crate::mu::r#type::NIL;
 // use crate::mu::r#type::entag;
 // use crate::mu::heap::heap;
 // use crate::mu::heap::Heap;
 
+use crate::mu::fixnum::_fixnum_add;
+
+type FuncV = fn(_args: Vec<Type>) -> Type;
+
 pub struct Env<'e> {
     // heap: Heap
-    _symtab: HashMap<&'e str, i32>
+    _symtab: HashMap<&'e str, FuncV>
 }
 
+// fn(args: Vec<Type>) -> Type
 pub fn env<'e>() -> Env<'e> {
-    let _init: HashMap<&'e str, i32> =
-        [("Norway", 100),
-         ("Denmark", 50),
-         ("Iceland", 10)]
-        .iter().cloned().collect();
-    
+    let mut _init: HashMap<&'e str, FuncV> = HashMap::new();
+
+    _init.insert("fixnum-add", _fixnum_add);
+
     Env {
         // heap: heap(1024 * 1024)
        _symtab: _init
     }
+}
+
+pub fn not_found(_args: Vec<Type>) -> Type {
+    NIL
 }
 
 impl Env<'_> {
@@ -35,10 +42,10 @@ impl Env<'_> {
         }
     }
 
-    pub fn lookup(&self, name: &str) -> i32 {
+    pub fn lookup(&self, name: &str) -> FuncV {
         match self._symtab.get(name) {
-            Some(number) => *number,
-            None => -1
+            Some(func) => *func,
+            None => not_found
         }
     }
 }
@@ -50,8 +57,8 @@ mod tests {
     #[test]
     fn test_symtab() {
         let env = env();
-        assert!(env.lookup(&"Norway") == 100);
-        assert!(env.lookup(&"nope") == -1);
+        assert!(env.lookup(&"fixnum-add") == _fixnum_add);
+        assert!(env.lookup(&"nope") == not_found);
     }
 }
 
