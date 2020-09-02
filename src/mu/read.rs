ab46b3a2c9fa9d0c42ff0920a4b4aa2ff1401509
James M. Putnam <putnamjm.design@gmail.com>
2020-09-02 15:17:51 -0400

    #38 - sexpr parser

--- src/mu/read.rs
+++ src/mu/read.rs
@@ -31,73 +31,45 @@ use nom::character::is_digit;
 
 use nom::character::*;
 
-/*
-// We parse any expr surrounded by parens, ignoring all whitespaces around those
-named!(parens<i64>, ws!(delimited!( tag!("("), expr, tag!(")") )) );
-
-// We transform an integer string into a i64, ignoring surrounding whitespaces
-// We look for a digit suite, and try to convert it.
-// If either str::from_utf8 or FromStr::from_str fail,
-// we fallback to the parens parser defined above
-named!(factor<i64>, alt!(
-    map_res!(
-      map_res!(
-        ws!(digit),
-        str::from_utf8
-      ),
-      FromStr::from_str
-    )
-  | parens
-  )
-);
-
-// We read an initial factor and for each time we find
-// a * or / operator followed by another factor, we do
-// the math by folding everything
-named!(term <i64>, do_parse!(
-    init: factor >>
-    res:  fold_many0!(
-        pair!(alt!(tag!("*") | tag!("/")), factor),
-        init,
-        |acc, (op, val): (&[u8], i64)| {
-            if (op[0] as char) == '*' { acc * val } else { acc / val }
-        }
-    ) >>
-    (res)
-  )
-);
- */
-
-named!(fx<&[u8], &[u8]>,
+named!(fixnum<&[u8], &[u8]>,
        alt!(complete!(take_while!(is_digit)) |
             complete!(ws!(take_while!(is_digit)))));
+
+named!(symbol<&[u8], &[u8]>,
+       alt!(complete!(take_while!(is_alphanumeric)) |
+            complete!(ws!(take_while!(is_alphanumeric)))));
 
 // pub fn _read(_src: Type) -> Type {
 pub fn _read() -> Type {
     let input = io::stdin().lock().lines().next().unwrap().unwrap();
 
-    match fx(input.as_bytes()) {
+    match fixnum(input.as_bytes()) {
         Ok((rest, token)) =>
-            {
-                println!("{:x?},{:x?}", rest, token);
-                match from_utf8(&token) {
-                    Ok(str) =>
-                        match i64::from_str(&str) {
-                            Ok(fix) => _fixnum(fix),
-                            Err(_) => NIL
-                        },
-                    Err(whoops) => 
+        {
+            // println!("{:x?},{:x?}", rest, token);
+            match from_utf8(&token) {
+                Ok(str) =>
+                    match i64::from_str(&str) {
+                        Ok(fix) =>
                         {
-                            println!("Err{:x?}", whoops);
-                            NIL
-                        }
+                            let fx = _fixnum(fix);
+                            println!("read fixnum: {:?}", fx);
+                            fx
+                        },
+                        Err(_) => NIL
+                    },
+                Err(whoops) => 
+                {
+                    println!("Err{:x?}", whoops);
+                    NIL
                 }
-            },
-        Err(whoops) =>
-            {
-                println!("{:x?}", whoops);
-                NIL
             }
+        },
+        Err(whoops) =>
+        {
+            println!("{:x?}", whoops);
+            NIL
+        }
     }
 }
 
