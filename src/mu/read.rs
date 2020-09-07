ef0ce5dd25dedc85cace8b245c5212f994ee58af
James M. Putnam <putnamjm.design@gmail.com>
2020-09-07 11:52:39 -0400

    #67 - improve sexpr parser

--- src/mu/read.rs
+++ src/mu/read.rs
@@ -7,8 +7,11 @@ use std::io::{self, BufRead};
 use std::str::FromStr;
 use std::str::from_utf8;
 
+use crate::mu::r#type::_immediate;
+use crate::mu::r#type::ImmediateClass;
 use crate::mu::r#type::Type;
 use crate::mu::r#type::NIL;
+use crate::mu::r#type::_T;
 
 use crate::mu::fixnum::_fixnum;
 use crate::mu::string::_string;
@@ -31,6 +34,14 @@ named!(symbol_<&[u8], (Option<&[u8]>, &[u8])>,
        tuple!(
            opt!(take_while!(is_space)),
            take_while!(is_alphanumeric)
+       )
+);
+
+named!(char_<&[u8], (Option<&[u8]>, &[u8], &[u8])>,
+       tuple!(
+           opt!(take_while!(is_space)),
+           tag!("#\\"),
+           take!(1)
        )
 );
 
@@ -64,6 +75,15 @@ named!(nil_<&[u8], (Option<&[u8]>, &[u8], Option<&[u8]>, &[u8])>,
 
 named!(read_<Type>, alt!(
 
+    char_ => { |cs: (Option<&[u8]>, &[u8], &[u8])| {
+        println!("char");
+        _immediate(cs.2[0] as u64,
+                   1,
+                   ImmediateClass::Char)
+    }
+    } |
+
+
     /*
     symbol_ => { |ss: (Option<&[u8]>, &[u8])|
                   match from_utf8(ss.1) {
@@ -76,38 +96,47 @@ named!(read_<Type>, alt!(
                       Err(_) => NIL
                   }
     } |
-
-*/
+     */
+    
+    /*
     string_ => { |ss: (Option<&[u8]>, &[u8], &[u8], &[u8])|
                   match from_utf8(ss.2) {
                       Ok(str) => _string(str.as_bytes()),
                       Err(_) => NIL
                   }
     } |
+     */
 
-    nil_ => { |_fs: (Option<&[u8]>, &[u8], Option<&[u8]>, &[u8])|
-                NIL
-    } |
-
-    cons_ => { |cs: (Option<&[u8]>, &[u8], Type, Option<&[u8]>, &[u8])|
-                Type::cons(cs.2, NIL)
-    } |
-
-    fixnum_ => { |fs: (Option<&[u8]>, &[u8])|
-                  match from_utf8(fs.1) {
-                      Ok(str) =>
-                          match i64::from_str(&str) {
-                              Ok(fix) =>
-                              {
-                                  let fx = _fixnum(fix);
-                                  println!("read fixnum: {:?}", fx);
-                                  fx
-                              },
-                              Err(_) => NIL
-                          },
-                      Err(_) => NIL
-                  }
+    nil_ => { |_fs: (Option<&[u8]>, &[u8], Option<&[u8]>, &[u8])| {
+        println!("nil");
+        NIL
     }
+    } |
+
+    cons_ => { |cs: (Option<&[u8]>, &[u8], Type, Option<&[u8]>, &[u8])| {
+        println!("cons");
+        cs.2.cons(NIL)
+    }
+    } |
+
+    fixnum_ => { |fs: (Option<&[u8]>, &[u8])| {
+        println!("fixnum");
+        match from_utf8(fs.1) {
+            Ok(str) =>
+                match i64::from_str(&str) {
+                    Ok(fix) =>
+                    {
+                        let fx = _fixnum(fix);
+                        println!("read fixnum: {:?}", fx);
+                        fx
+                    },
+                    Err(_) => NIL
+                },
+            Err(_) => NIL
+        }
+    }
+    }
+
 ));
 
 // pub fn _read(_src: Type) -> Type {
@@ -115,10 +144,7 @@ pub fn _read() -> Type {
     let input = io::stdin().lock().lines().next().unwrap().unwrap();
 
     match read_(input.as_bytes()) {
-        Ok((_, _type)) =>
-        {
-            _type
-        }
+        Ok((_, _type)) => _type,
         Err(_err) =>
         {
             println!("undecoded {}", _err);
@@ -168,7 +194,7 @@ mod tests {
             })}
 
     #[test]
-    fn test_symbol_() {
+    fn test_symbol() {
         assert!(
             match symbol_(b" abc123 ") {
                 Ok((_, (_, str))) =>
@@ -180,7 +206,7 @@ mod tests {
             })}
 
     #[test]
-    fn test_string_() {
+    fn test_string() {
         assert!(
             match string_(b"\"abc123\" ") {
                 Ok((_, (_, _, str, _))) =>
@@ -192,7 +218,15 @@ mod tests {
             })}
 
     #[test]
-    fn test_cons_() {
+    fn test_char() {
+        assert!(
+            match char_(b"#\\a ") {
+                Ok((_, (_, _, _ch))) => true,
+                Err(_) => false
+            })}
+ 
+    #[test]
+    fn test_cons() {
         assert!(
             match cons_(b" ( 123 ) ") {
                 Ok((_, (_, _, type_, _, _))) => type_.type_fixnum(),
@@ -200,7 +234,7 @@ mod tests {
             })}
 
     #[test]
-    fn test_nil_() {
+    fn test_nil() {
         assert!(
             match nil_(b" ( ) ") {
                 Ok((_, (_, _, _, _))) => true,
