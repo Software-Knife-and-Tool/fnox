9b9763893c4628ef04e5f04642cb0ba482a1c2da
putnamjm <putnamjm@sa.edu>
2020-09-10 21:05:09 -0400

    #77 - loader

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use mu::env::env;
 
 fn main() {
-    println!("lispox 0.0.3");
 
+    println!("lispox 0.0.4");
+    
+    match std::env::args().nth(1) {
+        Some(path) => println!(";;; loading {}", path),
+        None => ()
+    }
+    
     let e = env();
 
     loop {
