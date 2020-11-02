f22c6258b8b213dc0e0302b225cc8995cf4edac1
James M. Putnam <putnamjm.design@gmail.com>
2020-11-01 21:17:47 -0500

    #88 - rethink tagged pointer

--- src/mu/heap.rs
+++ src/mu/heap.rs
@@ -1,10 +1,12 @@
 /* mu/heap.rs */
+use crate::mu::r#type::{Tag};
 
-pub struct Heap {
-    nwords: u32,           // number of u64 words
+pub struct Heap<'a> {
+    nwords: usize,         // number of u64 words
     fname: &'static str,   // mapped file name
-    mmap: memmap::MmapMut, // mapped file segment
-    fence: u32             // allocation fence
+    // mmap: memmap::MmapMut, // mapped file segment
+    mmap: &'a mut Vec<u8>,         // on heap
+    fence: usize           // allocation fence
 }
 
 use memmap;
@@ -13,6 +15,12 @@ use std::{
     io::{Seek, SeekFrom, Write},
 };
 
+fn alloc(mut heap: &Heap, nwords: u16, tag: Tag) -> u64 {
+    let addr: u64 = unsafe { std::mem::transmute(b"1234") };
+    addr
+}
+
+/*
 fn mmap(size: u64, fname: &str) -> memmap::MmapMut {
     let mut map =
         OpenOptions::new()
@@ -26,40 +34,32 @@ fn mmap(size: u64, fname: &str) -> memmap::MmapMut {
     map.write_all(&[0]).unwrap();
     map.seek(SeekFrom::Start(0)).unwrap();
 
-    // let src = "Hello!";
-    // let mut data = 
-    //    unsafe {
-    //        memmap::MmapOptions::new()
-    //            .map_mut(&f)
-    //            .expect("could not access data from memory mapped file")
-    //    }
-    //
-    // data[..src.len()].copy_from_slice(src.as_bytes());
-
     unsafe {
         memmap::MmapOptions::new()
             .map_mut(&map)
-            .expect("unable not create heap image (mmap)")
+            .expect("unable to create heap image (mmap)")
     }
 }
+*/
 
-pub fn _heap(nwords: u32) -> Heap {
+pub fn _heap(nwords: usize) -> Heap {
     Heap {
         nwords,
         fname: "/var/tmp/lispox",
-        mmap: mmap((nwords * 8).into(), "/var/tmp/lispox"),
+        // mmap: mmap((nwords * 8).into(), "/var/tmp/lispox"),
+        mmap: Vec::with_capacity(nwords * 8),
         fence: 0
     }
 }
 
 impl Heap {
-    pub fn size(&self) -> u64 {
-        (self.nwords * 8).into()
+    pub fn size(&self) -> usize {
+        self.nwords
     }
     pub fn file_name(&self) -> &str {
         self.fname
     }
-    pub fn fence(&self) -> u32 {
+    pub fn next(&self) -> usize {
         self.fence
     }
 }
