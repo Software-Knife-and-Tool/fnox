4836e306d80f476ae4ce5b23c5f26f0afadf3fbf
putnamjm <putnamjm@sa.edu>
2020-11-06 21:14:10 -0500

    #81 - sharp reader macros

--- src/mu/read.rs
+++ src/mu/read.rs
@@ -16,83 +16,69 @@ use nom::{tag, take, take_until, take_while, take_while1, tuple};
 
 use nom::character::{is_alphanumeric, is_digit, is_space};
 
-named!(fixnum_<&[u8], (Option<&[u8]>, &[u8], Option<&[u8]>)>,
-       tuple!(
-           opt!(take_while!(is_space)),
-           take_while1!(is_digit),
-           opt!(eof!())
-       )
+named!(fixnum_<&[u8], &[u8]>,
+       take_while1!(is_digit)
 );
 
-named!(symbol_<&[u8], (Option<&[u8]>, &[u8])>,
-       tuple!(
-           opt!(take_while!(is_space)),
-           take_while1!(is_alphanumeric)
-       )
+named!(symbol_<&[u8], &[u8]>,
+       take_while1!(is_alphanumeric)
 );
 
-named!(keyword_<&[u8], (Option<&[u8]>, &[u8], &[u8])>,
+named!(keyword_<&[u8], (&[u8], &[u8])>,
        tuple!(
-           opt!(take_while!(is_space)),
            tag!(":"),
            take_while1!(is_alphanumeric)
        )
 );
 
-named!(char_<&[u8], (Option<&[u8]>, &[u8], &[u8])>,
+named!(char_<&[u8], (&[u8], &[u8])>,
        tuple!(
-           opt!(take_while!(is_space)),
            tag!("#\\"),
            take!(1)
        )
 );
 
-named!(string_<&[u8], (Option<&[u8]>, &[u8], &[u8], &[u8])>,
+named!(string_<&[u8], (&[u8], &[u8], &[u8])>,
        tuple!(
-           opt!(take_while!(is_space)),
            tag!("\""),
            take_until!("\""),
            tag!("\"")
        )
 );
 
-named!(cons_<&[u8], (Option<&[u8]>, &[u8], Type, Option<&[u8]>, &[u8])>,
+named!(cons_<&[u8], (&[u8], Type, Option<&[u8]>, &[u8])>,
        tuple!(
-           opt!(take_while!(is_space)),
            tag!("("),
-           read_,
+           type_,
            opt!(take_while!(is_space)),
            tag!(")")
        )
 );
 
-named!(dotted_<&[u8], (Option<&[u8]>, &[u8], Type, Option<&[u8]>, &[u8], Option<&[u8]>, Type, Option<&[u8]>, &[u8])>,
+named!(dotted_<&[u8], (&[u8], Type, Option<&[u8]>, &[u8], Option<&[u8]>, Type, Option<&[u8]>, &[u8])>,
        tuple!(
-           opt!(take_while!(is_space)),
            tag!("("),
-           read_,
+           type_,
            opt!(take_while!(is_space)),
            tag!("."),
            opt!(take_while!(is_space)),
-           read_,
+           type_,
            opt!(take_while!(is_space)),
            tag!(")")
        )
 );
 
-named!(list_<&[u8], (Option<&[u8]>, &[u8], Vec<Type>, Option<&[u8]>, &[u8])>,
+named!(list_<&[u8], (&[u8], Vec<Type>, Option<&[u8]>, &[u8])>,
        tuple!(
-           opt!(take_while!(is_space)),
            tag!("("),
-           many1!(read_),
+           many1!(type_),
            opt!(take_while!(is_space)),
            tag!(")")
        )
 );
 
-named!(nil_<&[u8], (Option<&[u8]>, &[u8], Option<&[u8]>, &[u8])>,
+named!(nil_<&[u8], (&[u8], Option<&[u8]>, &[u8])>,
        tuple!(
-           opt!(take_while!(is_space)),
            tag!("("),
            opt!(take_while!(is_space)),
            tag!(")")
@@ -100,17 +86,17 @@ named!(nil_<&[u8], (Option<&[u8]>, &[u8], Option<&[u8]>, &[u8])>,
 );
 
 named!(
-    read_<Type>,
+    type_<Type>,
     alt!(
-        char_ => { |cs: (Option<&[u8]>, &[u8], &[u8])|
-                    immediate(cs.2[0] as u64,
+        char_ => { |cs: (&[u8], &[u8])|
+                    immediate(cs.1[0] as u64,
                               1,
                               ImmediateClass::Char)
         } |
 
         /* distinguish fixnums from symbols */
-        fixnum_ => { |fs: (Option<&[u8]>, &[u8], Option<&[u8]>)|
-                      match from_utf8(fs.1) {
+        fixnum_ => { |fs: &[u8] |
+                      match from_utf8(fs) {
                           Ok(str) =>
                               match i64::from_str(&str) {
                                   Ok(fix) => fixnum(fix),
@@ -120,42 +106,50 @@ named!(
                       }
         } |
 
-        keyword_ => { |ks: (Option<&[u8]>, &[u8], &[u8])|
-                       keyword(_string(ks.2))
+        keyword_ => { |ks: (&[u8], &[u8]) |
+                       keyword(_string(ks.1))
         } |
 
-        symbol_ => { |ss: (Option<&[u8]>, &[u8])|
-                      symbol(_string(ss.1), NIL)
+        symbol_ => { |ss: &[u8]|
+                      symbol(_string(ss), NIL)
         } |
 
-        string_ => { |ss: (Option<&[u8]>, &[u8], &[u8], &[u8])|
-                      _string(ss.2)
+        string_ => { |ss: (&[u8], &[u8], &[u8])|
+                      _string(ss.1)
         } |
 
-        nil_ => { |_fs: (Option<&[u8]>, &[u8], Option<&[u8]>, &[u8])|
+        nil_ => { |_fs: (&[u8], Option<&[u8]>, &[u8])|
                     NIL
         } |
 
-        cons_ => { |cs: (Option<&[u8]>, &[u8], Type, Option<&[u8]>, &[u8])|
-                    cs.2.cons(NIL)
+        cons_ => { |cs: (&[u8], Type, Option<&[u8]>, &[u8])|
+                    cs.1.cons(NIL)
         } |
 
-        dotted_ => { |ds: (Option<&[u8]>, &[u8], Type, Option<&[u8]>, &[u8], Option<&[u8]>, Type, Option<&[u8]>, &[u8])|
-                       ds.2.cons(ds.6)
+        dotted_ => { |ds: (&[u8], Type, Option<&[u8]>, &[u8], Option<&[u8]>, Type, Option<&[u8]>, &[u8])|
+                       ds.1.cons(ds.5)
         } |
 
-        list_ => { |_ls: (Option<&[u8]>, &[u8], Vec<Type>, Option<&[u8]>, &[u8])|
+        list_ => { |_ls: (&[u8], Vec<Type>, Option<&[u8]>, &[u8])|
                     NIL
         }
     )
 );
 
+named!(read_form<&[u8], (Option<&[u8]>, Type, Option<&[u8]>)>,
+       tuple!(
+           opt!(take_while!(is_space)),
+           type_,
+           opt!(eof!())
+       )
+);
+
 pub fn _read() -> Type {
     let input = io::stdin().lock().lines().next().unwrap().unwrap();
     let instr = input.as_bytes();
-    
-    match read_(instr) {
-        Ok((_, type_)) => type_,
+
+    match read_form(instr) {
+        Ok((_, (_, type_, _))) => type_,
         Err(err) => {
             println!("undecoded {:?}", err);
             NIL
@@ -169,8 +163,8 @@ mod tests {
 
     #[test]
     fn test_fx() {
-        assert!(match fixnum_(b" 123 ") {
-            Ok((_, (_, fx, _))) => match from_utf8(fx) {
+        assert!(match fixnum_(b"123 ") {
+            Ok((_, fx)) => match from_utf8(fx) {
                 Ok(str) => match i64::from_str(&str) {
                     Ok(fix) => fix == 123,
                     Err(_) => false,
@@ -184,7 +178,7 @@ mod tests {
     #[test]
     fn test_fx1() {
         assert!(match fixnum_(b"123 ") {
-            Ok((_, (_, fx, _))) => match from_utf8(fx) {
+            Ok((_, fx)) => match from_utf8(fx) {
                 Ok(str) => match i64::from_str(&str) {
                     Ok(fix) => {
                         let _fx = fixnum(fix);
@@ -200,8 +194,8 @@ mod tests {
 
     #[test]
     fn test_symbol() {
-        assert!(match symbol_(b" abc123 ") {
-            Ok((_, (_, str))) => {
+        assert!(match symbol_(b"abc123 ") {
+            Ok((_, str)) => {
                 let _sy = symbol(_string(str), NIL);
                 true
             }
@@ -211,9 +205,9 @@ mod tests {
 
     #[test]
     fn test_keyword() {
-        assert!(match keyword_(b" :abc123 ") {
-            Ok((_, (_, _, str))) => {
-                let _kjw = keyword(_string(str));
+        assert!(match keyword_(b":abc123 ") {
+            Ok((_, (_, str))) => {
+                let _kw = keyword(_string(str));
                 true
             }
             Err(_) => false,
@@ -223,7 +217,7 @@ mod tests {
     #[test]
     fn test_string() {
         assert!(match string_(b"\"abc123\" ") {
-            Ok((_, (_, _, str, _))) => {
+            Ok((_, (_, str, _))) => {
                 let _st = _string(str);
                 true
             }
@@ -234,39 +228,39 @@ mod tests {
     #[test]
     fn test_char() {
         assert!(match char_(b"#\\a ") {
-            Ok((_, (_, _, _ch))) => true,
+            Ok((_, (_, _ch))) => true,
             Err(_) => false,
         })
     }
 
     #[test]
     fn test_cons() {
-        assert!(match cons_(b" ( 1234 ) ") {
-            Ok((_, (_, _, type_, _, _))) => type_.typep_fixnum(),
+        assert!(match cons_(b"( 1234 ) ") {
+            Ok((_, (_, type_, _, _))) => type_.typep_fixnum(),
             Err(_) => false,
         })
     }
 
     #[test]
     fn test_dotted() {
-        assert!(match dotted_(b" ( 123 . 456 ) ") {
-            Ok((_, (_, _, _car, _, _, _, _cdr, _, _))) => _car.typep_fixnum(),
+        assert!(match dotted_(b"( 123 . 456 ) ") {
+            Ok((_, (_, _car, _, _, _, _cdr, _, _))) => _car.typep_fixnum(),
             Err(_) => false,
         })
     }
 
     #[test]
     fn test_list() {
-        assert!(match list_(b" ( 1234 5678 ) ") {
-            Ok((_, (_, _, _vec, _, _))) => true,
+        assert!(match list_(b"( 1234 5678 ) ") {
+            Ok((_, (_, _vec, _, _))) => true,
             Err(_) => false,
         })
     }
 
     #[test]
     fn test_nil() {
-        assert!(match nil_(b" ( ) ") {
-            Ok((_, (_, _, _, _))) => true,
+        assert!(match nil_(b"( ) ") {
+            Ok((_, (_, _, _))) => true,
             Err(_) => false,
         })
     }
